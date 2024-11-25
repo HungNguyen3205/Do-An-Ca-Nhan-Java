19535951ef9dae850da552f2a2b0d3dfeb7c1e34
HungNguyen3205 <namhung03022005@gmail.com>
2024-11-25 19:24:17 +0700

    web cafe

---
+++ cafe-web-html/script.js
@@ -0,0 +1,7 @@
+let navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.add('active')
+}
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.remove('active')
+}
